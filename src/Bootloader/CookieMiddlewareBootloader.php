cc888d2bf0b780bec65510360dd0be44b6aebd7d
ncou <nicolas_courade@yahoo.fr>
2020-11-12 23:02:43 +0100

    update

--- src/Bootloader/CookieMiddlewareBootloader.php
+++ src/Bootloader/CookieMiddlewareBootloader.php
@@ -5,19 +5,19 @@ declare(strict_types=1);
 namespace Chiron\Cookies\Bootloader;
 
 use Chiron\Core\Container\Bootloader\AbstractBootloader;
-use Chiron\Http\MiddlewareQueue;
+use Chiron\Http\Http;
 use Chiron\Cookies\Middleware\CollectCookiesMiddleware;
 use Chiron\Cookies\Middleware\EncryptCookiesMiddleware;
 use Chiron\Cookies\Config\CookiesConfig;
 
 final class CookieMiddlewareBootloader extends AbstractBootloader
 {
-    public function boot(MiddlewareQueue $middlewares, CookiesConfig $config): void
+    public function boot(Http $http, CookiesConfig $config): void
     {
         if ($config->getEncrypt() === true) {
-            $middlewares->addMiddleware(EncryptCookiesMiddleware::class, MiddlewareQueue::PRIORITY_MAX - 10);
+            $http->addMiddleware(EncryptCookiesMiddleware::class, Http::PRIORITY_MAX - 10);
         }
 
-        $middlewares->addMiddleware(CollectCookiesMiddleware::class, MiddlewareQueue::PRIORITY_MAX - 20);
+        $http->addMiddleware(CollectCookiesMiddleware::class, Http::PRIORITY_MAX - 20);
     }
 }
