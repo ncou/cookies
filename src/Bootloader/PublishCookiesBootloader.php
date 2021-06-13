a70ac66a324b80c482e75e7e0af9b0019906e83d
ncou <nicolas_courade@yahoo.fr>
2021-06-13 17:25:10 +0200

    update

--- src/Bootloader/PublishCookiesBootloader.php
+++ src/Bootloader/PublishCookiesBootloader.php
@@ -6,7 +6,7 @@ namespace Chiron\Cookies\Bootloader;
 
 use Chiron\Core\Directories;
 use Chiron\Core\Container\Bootloader\AbstractBootloader;
-use Chiron\Core\Publisher;
+use Chiron\Publisher\Publisher;
 
 final class PublishCookiesBootloader extends AbstractBootloader
 {
